05600fada4f60b840753027816e8986b5e6edc96
sergiotravassos <sergiotravassos@outlook.com>
2022-07-16 17:57:49 +0100

    Projeto criado

---
+++ backend/src/main/java/com/bootcamp/javaspringreact/JavaspringreactApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.javaspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringreactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bootcamp/javaspringreact/JavaspringreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.javaspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

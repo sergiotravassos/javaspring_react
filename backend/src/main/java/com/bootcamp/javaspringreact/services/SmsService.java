374c5f25c826a4c186a8847a251be18f04c6d6d1
sergiotravassos <sergiotravassos@outlook.com>
2022-07-17 15:24:37 +0100

    Heroku

--- backend/src/main/java/com/bootcamp/javaspringreact/services/SmsService.java
+++ backend/src/main/java/com/bootcamp/javaspringreact/services/SmsService.java
@@ -43,7 +43,7 @@ public class SmsService {
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		Message message = Message.creator(to, from, "Teste").create();
+		Message message = Message.creator(to, from, msg).create();
 
 		System.out.println(message.getSid());
 	}

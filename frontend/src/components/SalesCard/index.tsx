9675bc63fb9f6d9f8bb55bb2057d21025ee8fdbb
sergiotravassos <sergiotravassos@outlook.com>
2022-07-17 17:44:23 +0100

    Envio de notificacao

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -77,7 +77,7 @@ function SalesCard() {
                                             <td>€ {sale.amount.toFixed(2)}</td>
                                             <td>
                                                 <div className="dsmeta-red-btn-container">
-                                                    <NotificationButton />
+                                                    <NotificationButton saleId={sale.id} />
                                                 </div>
                                             </td>
                                         </tr>
